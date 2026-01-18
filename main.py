a86e11a69180bac8350be0eaabb0929965ae2c67
mmoinsen <moinsen122@gmail.com>
2026-01-18 13:02:31 +0100

    Timezone fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, timezone
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///history.db'
@@ -10,15 +10,12 @@ db = SQLAlchemy(app)
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(3000), nullable=True)
-    
-    created_at = db.Column(db.DateTime(), default=datetime.now())
+    created_at = db.Column(db.DateTime(), default=datetime.utcnow)
 
 @app.route("/", methods=['GET','POST'])
 def start_page():
     if request.method == 'POST':
-        new_message = Message(
-            content=request.form['content']
-        )
+        new_message = Message(content=request.form['content'])
         db.session.add(new_message)
         db.session.commit()
     return render_template('index.html')
@@ -29,23 +26,24 @@ def get_History():
 
     history_data = []
     for msg in messages:
-        
         ts = 0.0
+        zeit_str = "--.--.----, --:-- Uhr"
+        
         if msg.created_at:
+            utc_dt = msg.created_at.replace(tzinfo=timezone.utc)
             
-            ts = msg.created_at.timestamp()
-           
-            zeit_str = msg.created_at.strftime("%d.%m.%Y, %H:%M Uhr")
-        else:
-            zeit_str = "--.--.----, --:-- Uhr"
+            local_dt = utc_dt.astimezone()
+            
+            zeit_str = local_dt.strftime("%d.%m.%Y, %H:%M Uhr")
+            
+            ts = utc_dt.timestamp()
 
         history_data.append({
-            'id': msg.id,           
-            'timestamp': ts,        
-            'time': zeit_str,       
+            'id': msg.id,
+            'timestamp': ts,
+            'time': zeit_str,  # Hier steht jetzt die korrekte deutsche Zeit drinnen!
             'message': msg.content
         })
-    
     return jsonify(history_data)
 
 @app.route("/delete_message/<int:id>", methods=['DELETE'])
